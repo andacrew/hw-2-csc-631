b87750f0dd7621c41519507a5d4ad7f4acbc8856
andrewcopas <andacrews@gmail.com>
2020-02-28 10:32:52 -0800

    First real commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+ppublic class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 500f;
+    public float strafeForce = 500f;
+
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey("w")){
+            rb.AddForce( 0, 0, forwardForce * Time.deltaTime);
+        }
+        if(Input.GetKey("a")){
+            rb.AddForce( -strafeForce * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey("s")){
+            rb.AddForce( 0, 0, -forwardForce * Time.deltaTime);
+        }
+        if(Input.GetKey("d")){
+            rb.AddForce( strafeForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
